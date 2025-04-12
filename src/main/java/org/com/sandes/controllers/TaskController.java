5070a565bc5db67a75a1100e0b4e9efb9b3dace0
Mateus Sandes <mateussandes@hotmail.com.br>
2025-04-12 20:55:24 -0300

    Atualizada o controller para suportar o servico

--- src/main/java/org/com/sandes/controllers/TaskController.java
+++ src/main/java/org/com/sandes/controllers/TaskController.java
@@ -1,12 +1,11 @@
 package org.com.sandes.controllers;
 
 import org.com.sandes.model.Task;
+import org.com.sandes.services.TaskService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,79 +13,34 @@ import java.util.UUID;
 @RequestMapping("tasks")
 public class TaskController {
 
-    final private List<Task> tasks = new ArrayList<>();
+    private TaskService service;
+
+    public TaskController(TaskService service) {
+        this.service = service;
+    }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Task> getTasks() {
-        return tasks;
-    }
-
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Task createTask(@RequestBody Task task) {
-        if (task == null) {
-            throw new RuntimeException("A tarefa não pode ser vazia!");
-        }
-
-        task.setId(UUID.randomUUID());
-        task.setDataCriacao(new Date());
-
-        tasks.add(task);
-
-        return task;
-    }
-
-    @PutMapping
-    public Task updateTask(@RequestBody Task task) {
-        if (task == null) {
-            throw new RuntimeException("A tarefa não pode ser vazia!");
-        }
-
-        for(Task aimTask: tasks) {
-            if(aimTask.getId().equals(task.getId())) {
-
-                aimTask.setTitulo(task.getTitulo());
-                aimTask.setDescricao(task.getDescricao());
-
-                return aimTask;
-            }
-        }
-
-        return null;
+        return service.findAll();
     }
 
     @GetMapping(value = "/{id}")
     public Task findById(@PathVariable UUID id) {
-        if(id == null) {
-            throw new RuntimeException("O id não pode ser nulo para a pesquisa!");
-        }
+        return service.findById(id);
+    }
 
-        for(Task aimTask: tasks) {
-            if(aimTask.getId().equals(id)) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Task createTask(@RequestBody Task task) {
+        return service.createTask(task);
+    }
 
-                return aimTask;
-            }
-        }
-
-        return null;
+    @PutMapping
+    public Task updateTask(@RequestBody Task task) {
+        return service.updateTask(task);
     }
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> deleteTasks(@PathVariable UUID id) {
-        if(id == null) {
-            throw new RuntimeException("O id não pode ser nulo para a pesquisa!");
-        }
-
-        for(Task aimTask: tasks) {
-            if(aimTask.getId().equals(id)) {
-
-                tasks.remove(aimTask);
-
-                System.out.println("Tarefa removida com sucesso!");
-
-                return ResponseEntity.noContent().build();
-            }
-        }
-
-        return null;
+        return service.deleteTask(id);
     }
 }
