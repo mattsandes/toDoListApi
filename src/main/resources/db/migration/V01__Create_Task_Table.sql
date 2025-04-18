e4f363cc7c8c837afc6efe3702eba3b121d2860c
Mateus Sandes <mateussandes@hotmail.com.br>
2025-04-17 21:51:36 -0300

    Criado a migration para criar a tabela quando a aplicacao iniciar

---
+++ src/main/resources/db/migration/V01__Create_Task_Table.sql
@@ -0,0 +1,18 @@
+-- Table: public.task_tb
+
+-- DROP TABLE IF EXISTS public.task_tb;
+
+CREATE TABLE IF NOT EXISTS public.task_tb
+(
+    id uuid NOT NULL,
+    titulo character varying(80) COLLATE pg_catalog."default" NOT NULL,
+    descricao character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    data_criacao timestamp(6) without time zone,
+    concluida boolean,
+    CONSTRAINT task_tb_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.task_tb
+    OWNER to postgres;
