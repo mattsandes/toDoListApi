4ae3df6bb9d9da5ac65ec7e90b3993eae025c13d
Mateus Sandes <mateussandes@hotmail.com.br>
2025-04-25 23:44:09 -0300

    Documentado os endepoints da API

--- src/main/java/org/com/sandes/controllers/TaskController.java
+++ src/main/java/org/com/sandes/controllers/TaskController.java
@@ -1,5 +1,10 @@
 package org.com.sandes.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.com.sandes.model.dtos.TaskDTO;
 import org.com.sandes.services.TaskService;
 import org.springframework.http.MediaType;
@@ -10,7 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("tasks")
+@RequestMapping("/api/tasks/v1")
 public class TaskController {
 
     private TaskService service;
@@ -20,26 +25,101 @@ public class TaskController {
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Endpoint para recuperar todas as tasks", description = "Retorna todas as tarefas criadas",
+    tags = {"Tasks"},
+    responses = {
+            @ApiResponse(description = "Sucesso", responseCode = "200",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = TaskDTO.class))
+                    )
+            }),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+    })
     public List<TaskDTO> getTasks() {
         return service.findAll();
     }
 
     @GetMapping(value = "/{id}")
+    @Operation(summary = "Endpoint para recuperar tarefas com base no id delas", description = "Retorna tarefas criadas com base no seus id",
+            tags = {"Tasks"},
+            responses = {
+                    @ApiResponse(description = "Sucesso", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = TaskDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            })
     public TaskDTO findById(@PathVariable UUID id) {
         return service.findById(id);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Endpoint para criar novas tarefas", description = "Cria tarefas e as retorna ao usuário",
+            tags = {"Tasks"},
+            responses = {
+                    @ApiResponse(description = "Sucesso", responseCode = "201",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = TaskDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            })
     public TaskDTO createTask(@RequestBody TaskDTO dto) {
         return service.createTask(dto);
     }
 
     @PutMapping
+    @Operation(summary = "Endpoint para atualizar dados das tarefas", description = "Atualiza tarefas e as retorna para o usuário",
+            tags = {"Tasks"},
+            responses = {
+                    @ApiResponse(description = "Sucesso", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = TaskDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            })
     public TaskDTO updateTask(@RequestBody TaskDTO task) {
         return service.updateTask(task);
     }
 
     @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Endpoint para apagar tarefas da base de dados", description = "Apaga tarefas com base nos seus ids",
+            tags = {"Tasks"},
+            responses = {
+                    @ApiResponse(description = "Sucesso", responseCode = "204",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = TaskDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            })
     public ResponseEntity<?> deleteTasks(@PathVariable UUID id) {
         return service.deleteTask(id);
     }
