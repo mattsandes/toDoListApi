5836a95e5b2d4083bb4bcadf96b0566d8d1a0e79
Mateus Sandes <mateussandes@hotmail.com.br>
2025-05-08 18:48:51 -0300

    Criada o enum com as roles do usuários

---
+++ src/main/java/org/com/sandes/model/UserRole.java
@@ -0,0 +1,17 @@
+package org.com.sandes.model;
+
+public enum UserRole {
+
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole(){
+        return role;
+    }
+}
