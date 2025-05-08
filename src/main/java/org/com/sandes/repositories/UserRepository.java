0e156491102a3f535e5705207e0492deb8737a0e
Mateus Sandes <mateussandes@hotmail.com.br>
2025-05-08 18:56:22 -0300

    Criado o repositorio pra usuários com o metodo findByLogin

---
+++ src/main/java/org/com/sandes/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package org.com.sandes.repositories;
+
+import org.com.sandes.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    UserDetails findByLogin(String login);
+}
